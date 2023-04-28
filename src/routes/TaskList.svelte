b1d413cbac3cb0d4adb48b8adaf2e14b3c4919af
PabloTercerosC <pablo.terceros.60104@usalesiana.edu.bo>
2023-04-28 01:08:29 -0400

    edit list implementation

--- src/routes/TaskList.svelte
+++ src/routes/TaskList.svelte
@@ -8,12 +8,43 @@
         target: event.target.parentNode.querySelector('.ul-listTasks'),
       });
     }
-    editMode = 0;
   }
+
+  function save() {
+        const parent = this.parentElement;
+
+        const inputs = parent.querySelectorAll('input');
+        const labels = parent.querySelectorAll('label');
+        const buttons = parent.querySelectorAll('button');
+
+        if (inputs[0].value == "") {
+            inputs[0].value = labels[0].textContent;
+        } else {
+            labels[0].textContent = inputs[0].value;
+        }
+
+        buttons[1].style.display = "none";
+        inputs[0].style.display = "none";
+    }
+
+    function show() {
+        const parent = this.parentElement;
+
+        const inputs = parent.querySelectorAll('input');
+        const labels = parent.querySelectorAll('label');
+        const buttons = parent.querySelectorAll('button');
+
+        inputs[0].value = labels[0].textContent
+
+        buttons[1].style.display = "inline";
+        inputs[0].style.display = "inline";
+    }
 </script>
 
 <div class="list bg-[#A9907E] rounded-[10PX] w-1/2 p-4 mb-4">
-  <li class="title-List font-bold">{name}</li>
+  <label class="title-List font-bold" on:click={show}>{name}</label>
   <button class="button-AddTask bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" type="button" on:click={addNewTask}>Add task</button>
+  <input type="text" style="display: none;">
+  <button on:click={save} style="display: none;">Done</button>
   <ul class="ul-listTasks"></ul>
 </div>
