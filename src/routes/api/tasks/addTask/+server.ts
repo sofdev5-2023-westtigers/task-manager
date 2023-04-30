2da4e4cfd535cf60c9ce0be8820a2c9ecca92f5b
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2023-04-30 15:32:11 -0400

    edit taskName in database

--- src/routes/api/tasks/addTask/+server.ts
+++ src/routes/api/tasks/addTask/+server.ts
@@ -12,3 +12,19 @@ export const POST : RequestHandler = (async ({request,locals}) => {
 
     return json(insertedList);
 });
+
+export const PUT: RequestHandler = async ({ request, locals }) => {
+    const body = await request.formData();
+    const list = {userId: body.get('userId'), taskNameOld: body.get('taskNameOld'), taskName: body.get('taskName'), listName: body.get('listName')};
+
+    const result = await tasks.updateOne(
+        { userId: body.get('userId'), taskName:  body.get('taskNameOld'), },
+        { $set: { taskName: list.taskName} }
+      );
+    
+    const updatedList = await tasks.findOne({ taskName: list.taskName});
+    
+    console.log(updatedList, locals);
+    
+    return json(updatedList);
+  };
