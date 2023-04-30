b0d7d047da52a239851dc07ad17062bdcad6546b
melanyArgandona <melmune@gmail.com>
2023-04-30 03:43:04 -0400

    implement classes necesaries for calendar and diferent options for radio buttons

---
+++ src/lib/components/calendar/DatePick.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import OptionsDate from './OptionsDate.svelte';
+    export let showRadioButtons = false;
+
+    function handleClick() {
+        showRadioButtons = !showRadioButtons;
+    }
+
+    onMount(() => {
+        document.addEventListener('click', (event) => {
+            if (!event.target.closest('.date-input-container')) {
+                showRadioButtons = false;
+            }
+        });
+    });
+</script>
+
+<div class="date-input-container">
+    <input type="text" placeholder="Select a date…" class="input rounded-[2PX] w-1 px-1 py-1 mt-2 text-sm" on:click={handleClick} class:hidden={showRadioButtons} />
+
+    {#if showRadioButtons}
+        <OptionsDate/>
+    {/if}
+</div>
---
+++ src/lib/components/calendar/SelectDaysCalendar.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+    import "carbon-components-svelte/css/white.css"
+    export let selectedDatesValue = '';
+
+    function handleDateChange(event) {
+        const { selectedDates: [dateFrom], dateStr: { from, to } } = event.detail;
+        selectedDatesValue = from + ' - ' + to;
+    }
+</script>
+
+<DatePicker datePickerType="range" on:change on:change={handleDateChange}>
+    <DatePickerInput labelText="Start Task" placeholder="mm/dd/yyyy" />
+    <DatePickerInput labelText="End Task" placeholder="mm/dd/yyyy" />
+</DatePicker>
