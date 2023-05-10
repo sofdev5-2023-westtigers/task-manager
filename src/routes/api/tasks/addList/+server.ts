f6daee835421cd0f5d58e4ddea83bae5efcb8c22
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2023-05-09 22:12:43 -0400

    sepair into diferents folders the data apis

--- src/routes/api/tasks/addList/+server.ts
+++ src/routes/api/tasks/addList/+server.ts
@@ -1,5 +1,4 @@
 import { lists } from "$db/lists";
-import { tasks } from "$db/tasks";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const POST : RequestHandler = (async ({request,locals}) => {
@@ -7,33 +6,5 @@ export const POST : RequestHandler = (async ({request,locals}) => {
     const list = {listName: body.get('listName')};
     const result = await lists.insertOne(list);
     const insertedList = await lists.findOne({_id : result.insertedId});
-
-    // log all fields
-
     return json(insertedList);
 });
-
-export const PUT: RequestHandler = async ({ request, locals }) => {
-    const body = await request.formData();
-    const list = {userId: body.get('userId'), listNameOld: body.get('listNameOld'), listName: body.get('listName')};
-  
-    const result = await tasks.updateMany(
-        { listName:  body.get('listNameOld') },
-        { $set: { listName: list.listName} }
-      );
-      const result2 = await lists.updateOne(
-        { listName:  body.get('listNameOld') },
-        { $set: { listName: list.listName} }
-      );
-      const result3 = await tasks.updateMany(
-        { listName: list.listName },
-        { $set: { listName: list.listName } }
-      );
-
-    
-    const updatedList = await tasks.findOne({ listName: list.listName});
-    
-    console.log(updatedList, locals);
-    
-    return json(updatedList);
-  };
---
+++ src/routes/api/tasks/updateList/+server.ts
@@ -0,0 +1,27 @@
+import { lists } from "$db/lists";
+import { tasks } from "$db/tasks";
+import { json, type RequestHandler } from "@sveltejs/kit";
+export const PUT: RequestHandler = async ({ request, locals }) => {
+    const body = await request.formData();
+    const list = {userId: body.get('userId'), listNameOld: body.get('listNameOld'), listName: body.get('listName')};
+  
+    const result = await tasks.updateMany(
+        { listName:  body.get('listNameOld') },
+        { $set: { listName: list.listName} }
+      );
+      const result2 = await lists.updateOne(
+        { listName:  body.get('listNameOld') },
+        { $set: { listName: list.listName} }
+      );
+      const result3 = await tasks.updateMany(
+        { listName: list.listName },
+        { $set: { listName: list.listName } }
+      );
+
+    
+    const updatedList = await tasks.findOne({ listName: list.listName});
+    
+    console.log(updatedList, locals);
+    
+    return json(updatedList);
+  };
