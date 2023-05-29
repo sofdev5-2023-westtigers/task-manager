0aac0086f9614d383dfcd931360d7445b782a070
Valeria Valenzuela <Valeria.Valenzuela.60105@usalesiana.edu.bo>
2023-05-29 00:19:36 -0400

    added tests for board view included TaskBord and TaskCard components

---
+++ src/lib/components/tasks/TaskCard.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, it } from 'vitest';
+import {render, screen} from '@testing-library/svelte';
+import TaskCard from './TaskCard.svelte';
+
+describe('TaskCard component background', () => {
+	it('should render with flex-row style', () => {
+		render(TaskCard);
+		const clazz = screen.getByTestId('content').getAttribute('class');
+		expect(clazz).toContain('flex');
+        expect(clazz).toContain('flex-row');
+	});
+});
+
+describe('TaskCard components', () => {
+	it('should render with hidden attribute', () => {
+		render(TaskCard);
+		const inputName = screen.getByTestId('input') as HTMLInputElement;
+        const datePick = screen.getByTestId('datepick') as HTMLInputElement;
+		expect(inputName).toHaveProperty('hidden');
+        expect(datePick).toHaveProperty('hidden');
+	});
+});
+
+describe('TaskCard components', () => {
+	it('should render with none display style', () => {
+		render(TaskCard);
+		const inputName = screen.getByTestId('input-edit').getAttribute('style');
+        const buttonDone = screen.getByTestId('button-done').getAttribute('style');
+		expect(inputName).toContain('none');
+        expect(buttonDone).toContain('none');
+	});
+});
