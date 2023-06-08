40ba786715b8188ad982d5faa9f93f08f95a2354
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-06-08 03:08:20 -0400

    visual alert added to the control of names for the lists

--- src/lib/components/tasks/ListOfLists.svelte
+++ src/lib/components/tasks/ListOfLists.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+  import { onMount } from "svelte";
   import { Registry } from '$lib/auth/Registry';
 	import type { User } from '$lib/auth/User';
 	import TaskList from './TaskList.svelte';
@@ -84,6 +84,7 @@
   <div class="w-full sm:w-3/5">
     <div id="addNewList" class="addNewList mt-2 mb-4 sm:mb-0 flex items-center justify-center sm:justify-start" style="padding-left: 100px; padding-right: 100px">
       <input class="text-nameList border-gray-300 bg-gray-100 rounded-[20PX] w-full sm:w-auto px-4 py-2 mr-2 sm:mr-4" type="text" name="name" placeholder="Name List...">
+
       <button class="button-addList bg-[#ABC4AA] text-white px-4 py-2 rounded-md" type="button" on:click={createList}>Add List</button>
       {#if showAlert && nameError !== ''}
         <div class="alert mt-2 text-red-500 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
@@ -106,7 +107,12 @@
         </div>
       {/if}
     </div>
-    <div id="tasklist" class="taskList mt-2" style="padding-left: 100px; padding-right: 100px">      
+
+    <div id="tasklist" class="taskList mt-2" style="padding-left: 100px; padding-right: 100px">
+      <div>{#if showError}
+        <ErrorAlert {errorMessage}/>
+        <br>
+      {/if}</div>
     {#if !isToggled}
     <div class="flex flex-col">
       {#each groupedTasks as group}
@@ -124,7 +130,7 @@
         {#if group._id.userId && user && group._id.userId.toString() === user.userId.toString()}
           <div class="flex-item">
             <TaskBoard name={group._id.listName} inputValue={group.tasks}/>
-          </div> 
+          </div>
         {/if}
       {/each}
       {#each listTasks as list}
