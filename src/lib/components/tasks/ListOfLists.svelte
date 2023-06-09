5eecc85acee25c4200b738e648e0b2bc98aa628c
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-06-09 02:50:01 -0400

    add limit and alert for list name

--- src/lib/components/tasks/ListOfLists.svelte
+++ src/lib/components/tasks/ListOfLists.svelte
@@ -93,82 +93,59 @@
 	<span>Error! Task failed successfully.</span>
 </div>
 <div class="listTasks" style="padding-top:80px;">
-	<div class="w-full sm:w-3/5">
-		<div
-			id="addNewList"
-			class="addNewList mt-2 mb-4 sm:mb-0 flex items-center justify-center sm:justify-start"
-			style="padding-left: 100px; padding-right: 100px"
-		>
-			<input
-				class="text-nameList border-gray-300 bg-gray-100 rounded-[20PX] w-full sm:w-auto px-4 py-2 mr-2 sm:mr-4"
-				type="text"
-				name="name"
-				placeholder="Name List..."
-			/>
-
-			<button
-				class="button-addList bg-[#ABC4AA] text-white px-4 py-2 rounded-md"
-				type="button"
-				on:click={createList}>Add List</button
-			>
-			{#if showAlert && nameError !== ''}
-				<div
-					class="alert mt-2 text-red-500 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-				>
-					<span class="block sm:inline">{nameError}</span>
-				</div>
-			{/if}
-			{#if !isToggled}
-				<div class="form-control">
-					<label class="label cursor-pointer">
-						<input type="checkbox" class="toggle ml-2 mr-2" on:click={toggle} />
-						<span class="label-text">Board Mode</span>
-					</label>
-				</div>
-			{:else}
-				<div class="form-control">
-					<label class="label cursor-pointer">
-						<input type="checkbox" class="toggle ml-2 mr-2" on:click={toggle} checked />
-						<span class="label-text">Board Mode</span>
-					</label>
-				</div>
-			{/if}
-		</div>
-
-		<div id="tasklist" class="taskList mt-2" style="padding-left: 100px; padding-right: 100px">
-			<div>
-				{#if showError}
-					<ErrorAlert {errorMessage} />
-					<br />
-				{/if}
-			</div>
-			{#if !isToggled}
-				<div class="flex flex-col">
-					{#each groupedTasks as group}
-						{#if group._id.userId && user && group._id.userId.toString() === user.userId.toString()}
-							<TaskList name={group._id.listName} inputValue={group.tasks} />
-						{/if}
-					{/each}
-					{#each listTasks as list}
-						<TaskList key={list.id} name={list.name} />
-					{/each}
-				</div>
-			{:else}
-				<div class="flex flex-row">
-					{#each groupedTasks as group}
-						{#if group._id.userId && user && group._id.userId.toString() === user.userId.toString()}
-							<div class="flex-item">
-								<TaskBoard name={group._id.listName} inputValue={group.tasks} />
-							</div>
-						{/if}
-					{/each}
-					{#each listTasks as list}
-						<div class="flex-item">
-							<TaskBoard name={list.name} />
-						</div>
-					{/each}
-				</div>
-			{/if}
-		</div>
-	</div>
+  <div class="w-full sm:w-3/5">
+    <div id="addNewList" class="addNewList mt-2 mb-4 sm:mb-0 flex items-center justify-center sm:justify-start" style="padding-left: 100px; padding-right: 100px">
+      <input class="text-nameList border-gray-300 bg-gray-100 rounded-[20PX] w-full sm:w-auto px-4 py-2 mr-2 sm:mr-4" type="text" name="name" placeholder="Name List...">
+      <button class="button-addList bg-[#ABC4AA] text-white px-4 py-2 rounded-md" type="button" on:click={createList}>Add List</button>
+      {#if showAlert && nameError !== ''}
+        <div class="alert mt-2 text-red-500 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+          <span class="block sm:inline">{nameError}</span>
+        </div>
+      {/if}
+      {#if !isToggled}
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <input type="checkbox" class="toggle ml-2 mr-2" on:click={toggle}/>
+            <span class="label-text">Board Mode</span>
+          </label>
+        </div>
+      {:else}
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <input type="checkbox" class="toggle ml-2 mr-2" on:click={toggle} checked/>
+            <span class="label-text">Board Mode</span>
+          </label>
+        </div>
+      {/if}
+    </div>
+    <div id="tasklist" class="taskList mt-2" style="padding-left: 100px; padding-right: 100px">      
+    {#if !isToggled}
+    <div class="flex flex-col">
+      {#each groupedTasks as group}
+        {#if group._id.userId && user && group._id.userId.toString() === user.userId.toString()}
+        <TaskList name={group._id.listName} inputValue={group.tasks}/>
+        {/if}
+      {/each}
+      {#each listTasks as list}
+          <TaskList key={list.id} name={list.name}/>
+        {/each}
+    </div>
+    {:else}
+    <div class="flex flex-row">
+      {#each groupedTasks as group}
+        {#if group._id.userId && user && group._id.userId.toString() === user.userId.toString()}
+          <div class="flex-item">
+            <TaskBoard name={group._id.listName} inputValue={group.tasks}/>
+          </div> 
+        {/if}
+      {/each}
+      {#each listTasks as list}
+      <div class="flex-item">
+        <TaskBoard name={list.name}/>
+      </div>
+        {/each}
+    </div>
+    {/if}
+    </div>
+  </div>
 </div>
