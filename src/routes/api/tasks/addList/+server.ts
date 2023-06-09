74f140894d41d022daa3f286f4a87c9c869c336c
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-06-09 01:42:34 -0400

    add limit name to control to api list

--- src/routes/api/tasks/addList/+server.ts
+++ src/routes/api/tasks/addList/+server.ts
@@ -1,10 +1,23 @@
 import { lists } from "$db/lists";
-import { json, type RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler, createResponse } from "@sveltejs/kit";
 
-export const POST : RequestHandler = (async ({request,locals}) => {
-    const body = await request.formData();
-    const list = {listName: body.get('listName')};
+class ListNameExceedsMaxLengthError extends Error {
+  constructor() {
+    super("List name should not exceed 26 characters.");
+    this.name = "ListNameExceedsMaxLengthError";
+  }
+}
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+  const body = await request.formData();
+  const listName = body.get('listName');
+
+  if (listName && listName.length > 26) {
+    throw new ListNameExceedsMaxLengthError();
+  } else {
+    const list = { listName };
     const result = await lists.insertOne(list);
-    const insertedList = await lists.findOne({_id : result.insertedId});
+    const insertedList = await lists.findOne({ _id: result.insertedId });
     return json(insertedList);
-});
+  }
+};
