51713903f0a7f6f6884245e9f1adf7521bc4a83d
melanyArgandona <melmune@gmail.com>
2023-06-10 14:38:30 -0400

    added the same spaces for taskCards

--- src/lib/components/tasks/TaskBoard.svelte
+++ src/lib/components/tasks/TaskBoard.svelte
@@ -36,32 +36,33 @@
 	}
 </script>
 
-<!-- To-do -->
-<div class="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
+<div class=" top justify-center flex flex-wrap" style="padding-top: 100px; padding-left: 100px;">
 	<div data-testid="content-element" class="bg-white rounded px-2 py-2">
 		<!-- board category header -->
 		<label class="title-List bg-red-100 rounded font-bold text-2xl" on:click={handleClick}>{name}</label>
 		<!-- board card -->
 		<ul class="list-Task mt-2 list-none">
 		{#each inputValue as task}
-			{#if task}
-              {#if !task.dates && !task.date}
-              <TaskCard  name={task.taskName} isCompleted={task.isCompleted}/>
-              {/if}
-              {#if task.date}
-              <TaskCard  name={task.taskName} containsDate={true} dateValue={formatDate(task.date)} isCompleted={task.isCompleted}/>
-              {/if}
-              {#if task.dates}
-              <TaskCard  name={task.taskName} containsDates={true} isCompleted={task.isCompleted} dateValue={task.dates.map(dateString => formatDate(dateString)).join("-")}/>
-              {/if}
-            {/if}
+				{#if task}
+              		{#if !task.dates && !task.date}
+              			<TaskCard  name={task.taskName} isCompleted={task.isCompleted}/>
+              		{/if}
+              		{#if task.date}
+              			<TaskCard  name={task.taskName} containsDate={true} dateValue={formatDate(task.date)} isCompleted={task.isCompleted}/>
+              		{/if}
+              		{#if task.dates}
+              			<TaskCard  name={task.taskName} containsDates={true} isCompleted={task.isCompleted} dateValue={task.dates.map(dateString => formatDate(dateString)).join("-")}/>
+              		{/if}
+            	{/if}
 		{/each}
 		</ul>
 		<div class="flex flex-col mt-2 px-1">
 			<button class="button-AddTask text-gray-400 px-1 py-1 mt-2 rounded-md text-sm" type="button" on:click={addNewTask(event, name)}> + 	 New</button>
 			<div class="flex-col" >
 				<input data-testid="input-name-task" class="input-nameTask text-black border-gray-300 bg-gray-100 rounded-[20PX] px-2 py-1 mt-2 text-sm" hidden type="text" name="item1-textfield" placeholder="Name Task....">
-     	 			<DatePick/>
+     	 			<div class="datepick-select" hidden>
+						<DatePick/>
+					</div>
 				<button  data-testid="button-add" class="button-add bg-[#ABC4AA] text-black px-1 py-1 mt-2 rounded-md text-sm" hidden type="button" on:click={createTask(event, user, name, date, dates, showPickDate, showPickDates, setFalsePicks, taskList,true)}>Add</button>
 			</div>
 		</div>
