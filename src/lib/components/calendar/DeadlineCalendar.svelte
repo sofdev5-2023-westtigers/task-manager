07cfad21837a91779676419df404b996f94a1034
agonzalesb107 <130121539+agonzalesb107@users.noreply.github.com>
2023-06-12 20:18:46 -0400

    Revert "Feature/control date calendar"

--- src/lib/components/calendar/DeadlineCalendar.svelte
+++ src/lib/components/calendar/DeadlineCalendar.svelte
@@ -1,30 +1,12 @@
 <script>
     import { DatePicker, DatePickerInput } from "carbon-components-svelte";
+    import "carbon-components-svelte/css/white.css";
     import { setDate } from "./CalendarOptions";
-  
-    let currentDate = new Date().toISOString().slice(0, 10);
-    let isDateValid = true;
-  
     function handleDateChange(event) {
-      const { value, validity } = event.target;
-      const selectedDate = new Date(value).toISOString().slice(0, 10);
-      isDateValid = validity.valid && selectedDate > currentDate;
-  
-      if (isDateValid) {
-        setDate(selectedDate);
-      } else {
-        setDate("");
-      }
+        setDate(event.detail.dateStr);
     }
-  </script>
-  
-  <DatePicker class="mt-2" datePickerType="single">
-    <DatePickerInput
-      class="datepickerInput"
-      placeholder="mm/dd/yyyy"
-      invalid={!isDateValid}
-      invalidText="Invalid date"
-      on:input={handleDateChange}
-    />
-  </DatePicker>
-  
+</script>
+
+<DatePicker class="mt-2" datePickerType="single" on:change={handleDateChange}>
+    <DatePickerInput class="datepickerInput" placeholder="mm/dd/yyyy" />
+</DatePicker>
