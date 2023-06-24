223c14e85b12e67900d9af0e786fd423790ede21
melanyArgandona <melmune@gmail.com>
2023-06-24 15:00:25 -0400

    added dependecies in all files

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -5,7 +6,12 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -17,6 +23,7 @@ const config = {
 			$calendar: 'src/lib/components/calendar',
 			$calendarTasks: 'src/lib/components/calendarTask',
 			$db: 'src/db',
+			$mail: 'src/routes/api/mail/sendmail',
 			$stadistics: 'src/lib/components/stadistics'
 		}
 	}
