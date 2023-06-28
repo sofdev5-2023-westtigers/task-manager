bcadaa09e4f2bcfa75b1745f48146c74fcf0255b
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-06-27 23:07:25 -0400

    new style for the tasks added in the TaskCard

--- src/lib/components/tasks/TaskCard.svelte
+++ src/lib/components/tasks/TaskCard.svelte
@@ -37,7 +37,6 @@
     <div class="p-2 rounded shadow-sm border-gray-100 border-2">
         <div data-testid="content" class="flex flex-row place-items-center justify-between form-control">
             <input data-testid="input" type="text" hidden>
-            <label class="label-text" on:click={showTasks} >{name}</label>
             <input data-testid="input-edit" class="task-modified border-gray-300 bg-gray-100 rounded-[10PX] px-1 py-1 mt-2 text-sm" type="text" style="display: none;">
             <button data-testid="button-done" class="buttonDone bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" on:click={() => saveTask(event, user)} style="display: none;">Done</button>
             {#if isCompleted}
@@ -45,6 +44,9 @@
             {:else}
                 <input class="checkbox checkbox-accent" type="checkbox" name="task" on:change={() => saveTask(event, user)}/>
             {/if}
+            <label class="label-text" on:click={showTasks} >{name}</label>
+
+
         </div>
         {#if containsDate}
             <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{containsDate? dateValue : date}</span></i>
@@ -59,3 +61,35 @@
         </div>
     </div>
 </div>
+
+<style>
+    input:checked + label, input:checked + label + i {
+        text-decoration: line-through;
+    }
+    .p-2 {
+        background-color: #fff;
+        border: 3px solid #c4c3c3;
+        border-radius: 30px;
+        margin: 10px;
+        padding: 6px 8px;
+        display: flex;
+        align-items: center;
+    }
+
+    .label-text {
+        color: #675D50;
+        font-size: 18px;
+        line-height: 16px;
+        margin-top: 4px;
+        margin-right: 10px;
+        font-family: 'Canva Sans', sans-serif;
+    }
+     .grid {
+         background-color: #ffffff;
+     }
+     .checkbox{
+         margin-right: 10px;
+     }
+
+
+</style>
