0d8ef994fe187350d97a3b823a85403b312256c3
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-06-27 23:07:58 -0400

    new task style added in the task list

--- src/lib/components/tasks/TaskList.svelte
+++ src/lib/components/tasks/TaskList.svelte
@@ -52,35 +52,67 @@
       <DatePick/>
     </div>
     <button class="button-add bg-[#ABC4AA] text-black px-1 py-1 mt-2 rounded-md text-sm" hidden type="button" on:click={createTask(event, user, name, date, dates, showPickDate, showPickDates, setFalsePicks, taskList, false, false)}>Add</button>
-    <ul class="list-Task mt-2 list-none">
+    <ul class="list-Task">
       {#each inputValue as task}
         {#if task}
-          <div style="margin-bottom:2px;" class="mt-2">
+          <div style="margin-bottom:2px;" class="list-item">
             {#if JSON.parse(task.isCompleted)}
-              <input class="checkbox form-checkbox h-5 w-5 text-gray-600 rounded-lg align-middle" type="checkbox" name="task" on:change={() => saveTask(event, user)} checked>
+              <input class="checkbox checkbox-accent" type="checkbox" name="task" on:change={() => saveTask(event, user)} checked />
             {:else}
-              <input class="checkbox form-checkbox h-5 w-5 text-gray-600 rounded-lg align-middle" type="checkbox" name="task" on:change={() => saveTask(event, user)}>
+              <input class="checkbox checkbox-accent" type="checkbox" name="task" on:change={() => saveTask(event, user)}/>
             {/if}
-            <label class="label-task ml-2 text-xl"  for="task"  on:click={showTasks}>{task.taskName} </label>
-            <input class="task-modified border-gray-300 bg-gray-100 rounded-[10PX] w-1/6 px-1 py-1 mt-2 text-sm" type="text" style="display: none;">
-            <button class="buttonDone bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" on:click={saveTask(event, user)} style="display: none;">Done</button>
+            <label class="label-task ml-2 text-xl list-card-details"  for="task"  on:click={showTasks}>{task.taskName} </label>
+            <input class="task-modified border-gray-300 bg-gray-100 rounded-[10PX] w-1/6 px-1 py-1 mt-2 text-sm js-card-details" type="text">
+            <button class="buttonDone bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm js-card-details" on:click={saveTask(event, user)}>Done</button>
             {#if task.date}
               <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{formatDate(task.date)}</span></i>
-              <button  class="buttonDoneDate bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
+              <button  class="buttonDoneDate bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm js-card-details" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)}>Save</button>
             {/if}
             {#if task.dates}
               <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{task.dates.map(dateString => formatDate(dateString)).join("-")}</span></i>
-              <button  class="buttonDoneDates bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
+              <button  class="buttonDoneDates bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm js-card-details" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)}>Save</button>
             {/if}
           </div>
         {/if}
       {/each}
     </ul>
+
   </li>
 </div>
 
 <style>
   input:checked + label, input:checked + label + i {
-      text-decoration: line-through;
+    text-decoration: line-through;
+  }
+  .list-Task {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .list-item {
+    background-color: #fff;
+    border: 3px solid #c4c3c3;
+    border-radius: 30px;
+    margin: 20px;
+    padding: 6px 8px;
+    display: flex;
+    align-items: center;
+  }
+
+  .list-card-details {
+    color: #675D50;
+    font-size: 18px;
+    line-height: 16px;
+    margin-top: 4px;
+    font-family: 'Canva Sans', sans-serif;
+  }
+
+  .js-card-details {
+    display: none;
+  }
+
+  .checkbox{
+    margin-right: 10px;
   }
 </style>
