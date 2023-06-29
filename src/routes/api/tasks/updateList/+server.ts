7c80b355a4269960d3579939127d0fa9d15441e3
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-06-29 02:45:29 -0400

    add members to api

--- src/routes/api/tasks/updateList/+server.ts
+++ src/routes/api/tasks/updateList/+server.ts
@@ -1,27 +1,41 @@
 import { lists } from "$db/lists";
 import { tasks } from "$db/tasks";
 import { json, type RequestHandler } from "@sveltejs/kit";
-export const PUT: RequestHandler = async ({ request, locals }) => {
-    const body = await request.formData();
-    const list = {userId: body.get('userId'), listNameOld: body.get('listNameOld'), listName: body.get('listName')};
-  
-    const result = await tasks.updateMany(
-        { listName:  body.get('listNameOld') },
-        { $set: { listName: list.listName} }
-      );
-      const result2 = await lists.updateOne(
-        { listName:  body.get('listNameOld') },
-        { $set: { listName: list.listName} }
-      );
-      const result3 = await tasks.updateMany(
-        { listName: list.listName },
-        { $set: { listName: list.listName } }
-      );
+import { getMailUsers } from "$lib/keycloak/getUserList";
 
-    
-    const updatedList = await tasks.findOne({ listName: list.listName});
-    
-    console.log(updatedList, locals);
-    
-    return json(updatedList);
-  };
+export const PUT: RequestHandler = async ({ request, locals }) => {
+  const body = await request.formData();
+  const listMembers: String[] = body.getAll('listMembers[]');
+  const users = await getMailUsers();
+
+  const members = listMembers
+    .map(id => users.find(user => user.id === id))
+    .filter(objetoEncontrado => objetoEncontrado !== undefined);
+
+
+  const list = { userId: body.get('userId'), listNameOld: body.get('listNameOld'), listName: body.get('listName'), listMembers: members };
+
+  const result = await tasks.updateMany(
+    { listName: body.get('listNameOld') },
+    { $set: { listName: list.listName } }
+  );
+  const result2 = await lists.updateOne(
+    { listName: body.get('listNameOld') },
+    { $set: { listName: list.listName } }
+  );
+  const result3 = await tasks.updateMany(
+    { listName: list.listName },
+    { $set: { listName: list.listName } }
+  );
+  const result4 = await tasks.updateMany(
+    { listMembers: list.listMembers },
+    { $set: {listMembers: list.listMembers } }
+  );
+
+
+  const updatedList = await tasks.findOne({ listName: list.listName });
+
+  console.log(updatedList, locals);
+
+  return json(updatedList);
+};
--- src/routes/api/tasks/updateTasks/+server.ts
+++ src/routes/api/tasks/updateTasks/+server.ts
@@ -1,16 +1,28 @@
 import { tasks } from "$db/tasks";
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { parseDate, parseDates } from "../addTask/DateParse";
+import { getMailUsers } from "$lib/keycloak/getUserList";
 export const PUT: RequestHandler = async ({ request, locals }) => {
     const body = await request.formData();
     const listCalendar = {userId: body.get('userId'), oldDates: body.get('oldDates'), dates: body.get('dates'), oldDate: body.get('oldDate'), date: body.get('date'), modifyDate: body.get('modifyDate')};
     const list = {userId: body.get('userId'), taskNameOld: body.get('taskNameOld'), taskName: body.get('taskName'), listName: body.get('listName'), isCompletedOld: body.get('isCompletedOld'),isCompleted: body.get('isCompleted')};
     const listTimeChronometer = {userId: body.get('userId'), oldTimeChronometer: body.get('oldTimeChronometer'), timeChronometer: body.get('timeChronometer')};
+    const listMembers: String[] = body.getAll('listMembers[]');
+    const users = await getMailUsers();
+
+    const members = listMembers
+        .map(id => users.find(user => user.id === id))
+        .filter(objetoEncontrado => objetoEncontrado !== undefined);
 
     const result = await tasks.updateOne(
         { userId: body.get('userId'), taskName:  body.get('taskNameOld')},
         { $set: { taskName: list.taskName, isCompleted: list.isCompleted == "true"}}
       );
+
+    const result2 = await tasks.updateOne(
+      { userId: body.get('userId'), taskName:  body.get('taskNameOld')},
+      { $set: { taskName: list.taskName, TaskMembers: members}}
+    );
 
       if (body.get('modifyDate') === "true") {
       const resultDates = await tasks.updateOne(
@@ -32,6 +44,7 @@ export const PUT: RequestHandler = async ({ request, locals }) => {
       }
   
     const updatedList = await tasks.findOne({ taskName: list.taskName});
+    console.log("Update",updatedList);
     
     return json(updatedList);
   };
