271431665ca293104fbafd8de537e830f408e4bc
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-06-30 04:29:23 -0400

    modal added for the date pick edit

--- src/lib/components/tasks/TaskCard.svelte
+++ src/lib/components/tasks/TaskCard.svelte
@@ -15,6 +15,7 @@
     let user: User;
     let prevDate : any;
     let prevDates : any;
+    let modalOpen = false;
 
     onMount(() => {
         Registry.auth.getUser().subscribe((data: User) => {
@@ -26,11 +27,17 @@
         showCalendar(event);
         prevDate = date;
         prevDates = dates;
+        modalOpen = true;
+    }
+
+    function closeModal() {
+        modalOpen = false;
     }
 </script>
 
+
 <svelte:head>
-	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+    <link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
 </svelte:head>
 
 <div class="grid grid-rows-1 gap-2">
@@ -48,18 +55,34 @@
         </div>
         {#if containsDate}
             <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{containsDate? dateValue : date}</span></i>
-            <button class="buttonDoneDate bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
         {/if}
         {#if containsDates}
             <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{containsDates? dateValue : dates}</span></i>
-            <button class="buttonDoneDates bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
         {/if}
-        <div data-testid="datepick" class="datepick-select" hidden>
-            <DatePick/>
+    </div>
+    <div
+            id="popup-modal"
+            tabindex="-1"
+            class={`fixed top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full ${modalOpen ? '' : 'hidden'}`}
+    >
+        <div class="relative w-64 h-[80%] max-w-md max-h-full overflow-auto border-4 border-black">
+            <div class="relative bg-white rounded-lg shadow dark">
+                <button type="button" on:click={closeModal} class="modal-close-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
+                <div class="p-8 text-center">
+                    <h3 class="modal-heading">Update date</h3>
+                    <DatePick class="date-pick"/>
+                    <button class="modal-button" name="save" type="button" on:click={(event) => {saveCalendar(event, user, date, dates, prevDate, prevDates); closeModal();}}>Save</button>
+                </div>
+            </div>
         </div>
     </div>
 
 </div>
+
 <style>
     .grid {
         border: 3px solid #c4c3c3;
@@ -70,5 +93,32 @@
         color: #675D50;
         font-family: 'Canva Sans', sans-serif;
     }
+    .modal-heading {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        font-size: 1.5rem;
+        font-weight: normal;
+        color: #666;
+    }
+    .modal-button {
+        background-color: #c4bcbc;
+        color: black;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        font-size: 1rem;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+    .modal-close-button {
+        position: absolute;
+        color: #000000;
+        background-color: transparent;
+        border-radius: 1rem;
+        right: 0.5rem;
+    }
+    .relative {
+        border: 1px solid #000000;
 
+        border-radius: 10px;
+    }
 </style>
