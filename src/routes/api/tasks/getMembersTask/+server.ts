e7a17880029df8a60c472b5c0ab9b457292a6112
Alejandro SerranoSelaya <alejandro.serrano.60103@usalesiana.edu.bo>
2023-06-30 12:07:09 -0400

    get Members

---
+++ src/routes/api/tasks/getMembersTask/+server.ts
@@ -0,0 +1,14 @@
+import { tasks } from "$db/tasks";
+import type { RequestHandler } from "@sveltejs/kit";
+import { json } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ request, locals }) => {
+    const body = await request.formData();
+    const list = {taskName: body.get('taskName'), listName: body.get('listName') };
+
+
+    let taskAux = await tasks.findOne({ listName: list.listName, taskName: list.taskName });
+    let membersList = taskAux.TaskMembers;
+    
+    return json(membersList);
+};
