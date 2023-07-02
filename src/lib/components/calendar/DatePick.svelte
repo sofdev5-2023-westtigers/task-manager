659696fec87bc3a444cdd787f35fd6f591c9f5eb
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-07-01 20:13:25 -0400

    new date picker design added

--- src/lib/components/calendar/DatePick.svelte
+++ src/lib/components/calendar/DatePick.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
+    import {onMount} from 'svelte';
     import OptionsDate from './OptionsDate.svelte';
+
     let showRadioButtons = false;
 
     function handleClick() {
@@ -17,8 +18,32 @@
 </script>
 
 <div class="date-input-container">
-    <input type="text" placeholder="Select a date…" class="input input-bordered mt-2" style="width: 200px;" on:click={handleClick} class:hidden={showRadioButtons} />
+    <button type="button" class="date-button" on:click={handleClick}>
+        <i class="mi mi-calendar"></i>
+    </button>
     {#if showRadioButtons}
         <OptionsDate/>
     {/if}
 </div>
+
+<style>
+    .date-input-container {
+        margin: 1px;
+        position: relative;
+        display: inline-block;
+    }
+
+    .date-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid black;
+        color: black;
+        background-color: white;
+        width: 2em;
+        height: 2em;
+        border-radius: 30px;
+        transition: background-color 0.3s ease, color 0.3s ease;
+    }
+
+</style>
