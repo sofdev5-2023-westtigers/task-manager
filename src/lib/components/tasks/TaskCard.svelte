d77db87f335b2ecb3b7356a2f1b889a32c88e5c5
Alejandro Espinoza <alejandro.espinoza.60092@usalesiana.edu.bo>
2023-07-02 14:23:43 -0400

    new datePick desgin added

--- src/lib/components/tasks/TaskCard.svelte
+++ src/lib/components/tasks/TaskCard.svelte
@@ -15,6 +15,7 @@
     let user: User;
     let prevDate : any;
     let prevDates : any;
+    let showDatePicker = false;
 
     onMount(() => {
         Registry.auth.getUser().subscribe((data: User) => {
@@ -26,19 +27,29 @@
         showCalendar(event);
         prevDate = date;
         prevDates = dates;
+        showDatePicker = !showDatePicker;
+    }
+    function cancelUpdate() {
+        showDatePicker = false;
+    }
+    function saveAndUpdate(event) {
+        saveCalendar(event, user, date, dates, prevDate, prevDates);
+        cancelUpdate();
     }
 </script>
 
+
 <svelte:head>
-	<link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
+    <link rel="stylesheet" href="https://unpkg.com/mono-icons@1.0.5/iconfont/icons.css" >
 </svelte:head>
+
 
 <div class="grid grid-rows-1 gap-2">
     <div class="p-2 rounded shadow-sm border-gray-100 border-2">
         <div data-testid="content" class="flex flex-row place-items-center justify-between form-control">
             <input data-testid="input" type="text" hidden>
             <label class="label-text" on:click={showTasks} >{name}</label>
-            <input data-testid="input-edit" class="task-modified border-gray-300 bg-gray-100 rounded-[10PX] px-1 py-1 mt-2 text-sm" type="text" style="display: none;">
+            <input data-testid="input-edit" class="task-modified border-gray-300 bg-gray-100 rounded-[10PX] px-1 py-1 mt-2 text-sm" type="text" style="display: none; width: 120px;">
             <button data-testid="button-done" class="buttonDone bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" on:click={() => saveTask(event, user)} style="display: none;">Done</button>
             {#if isCompleted}
                 <input class="checkbox checkbox-accent" type="checkbox" name="task" on:change={() => saveTask(event, user)} checked />
@@ -46,16 +57,46 @@
                 <input class="checkbox checkbox-accent" type="checkbox" name="task" on:change={() => saveTask(event, user)}/>
             {/if}
         </div>
-        {#if containsDate}
+        {#if containsDate || containsDates}
             <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{containsDate? dateValue : date}</span></i>
-            <button class="buttonDoneDate bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
+            {#if showDatePicker}
+                <div data-testid="datepick" class="datepick-select">
+                    <DatePick/>
+                </div>
+                <button class="buttonSaveDate px-2 py-1 rounded-full text-xs font-medium" name="save" type="button" on:click={saveAndUpdate}>Save</button>
+                <button class="buttonCancelDate px-2 py-1 rounded-full text-xs font-medium" name="cancel" type="button" on:click={cancelUpdate}>Cancel</button>
+            {/if}
         {/if}
-        {#if containsDates}
-            <i class="mi mi-calendar"><span class="u-sr-only" on:click={(event) => updateCalendar(event)}>{containsDates? dateValue : dates}</span></i>
-            <button class="buttonDoneDates bg-[#c4bcbc] text-black px-1 py-1 rounded-md text-sm" name="save" type="button" on:click={(event) => saveCalendar(event, user, date, dates, prevDate, prevDates)} hidden>Save</button>
-        {/if}
-        <div data-testid="datepick" class="datepick-select" hidden>
-            <DatePick/>
-        </div>
     </div>
 </div>
+
+<style>
+    .grid {
+        border: 3px solid #c4c3c3;
+        border-radius: 30px;
+        margin-top: 10px;
+    }
+    .label-text {
+        color: #675D50;
+        font-family: 'Canva Sans', sans-serif;
+    }
+    .buttonSaveDate {
+        margin-top: 1px;
+        color: #fff;
+        background-color: rgba(173, 244, 143, 0.69);
+
+    }
+    .buttonCancelDate {
+        margin-top: 1px;
+        color: #fff;
+        background-color: #f00;
+    }
+
+    .datepick-select {
+        margin: 2px;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
+
+</style>
